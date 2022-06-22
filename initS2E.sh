b70b15ccbfa1c1efb0afd2c744a8d8b63c603761
Lisong Xu <xu@unl.edu>
2022-06-22 16:31:56 +1000

    split into two scripts

--- initS2E.sh
+++ initS2E.sh
@@ -21,11 +21,3 @@ mkdir ../s2e
 cd ../s2e
 s2e init
 s2e build
-
-#replace launch.sh and images.json to build image
-wget -O source/guest-images/Linux/s2e_home/launch.sh https://raw.githubusercontent.com/JeffShao96/Symbolic-NS3/master/launch.sh
-wget -O source/guest-images/images.json https://raw.githubusercontent.com/JeffShao96/Symbolic-NS3/master/images.json
-
-# Give the authority to S2E.
-sudo usermod -a -G kvm $(whoami)
-sudo chmod ugo+r /boot/vmlinu*
